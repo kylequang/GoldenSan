9d3465f33b583a200408c81483915846d0c2c1b5
kyle <ky.le22@student.passerellesnumeriques.org>
2022-03-09 09:53:12 +0700

    config redirect role

--- App.js
+++ App.js
@@ -1,38 +1,13 @@
-import { StyleSheet } from 'react-native';
 
-import { NavigationContainer, StackActions } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './src/screens/auth/LoginScreen';
-import HomeScreen from './src/screens/home/HomeScreen';
-import PhoneNumber from './src/screens/auth/PhoneNumber';
-import Information from './src/screens/auth/Information';
-import Loading from './src/components/animation/Loading';
-import RepairmenLoading from './src/components/animation/RepairmenLoading';
-import Fixer from './src/screens/home/Fixer';
-import Adv from './src/components/animation/Adv';
-import Index from './src/screens/client/IndexClient';
-import DetailCategory from './src/screens/client/DetailCategory';
-import BottomTab from './src/navigation/client/BottomTab';
-import Manage from './src/screens/client/Manage';
-import HeaderUser from './src/components/HeaderUser';
-import ListRepairmen from './src/screens/client/ListRepairmen';
-import DetailRepairmen from './src/screens/client/DetailRepairmen';
 import { useEffect, useState } from 'react';
-import LoginFB from './src/screens/auth/LoginFB';
-
+import { LogBox  } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import RepairmenLoading from './src/components/animation/RepairmenLoading';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import Phone from './src/screens/auth/Phone';
-import BookOrder from './src/screens/client/BookOrder';
-import UploadImg from './src/screens/auth/UploadImg';
-import TakeCamera from './src/screens/auth/TakeCamera';
 import RoleStack from './src/navigation/RoleStack';
-
-import { LogBox } from 'react-native';
 import AuthScreen from './src/navigation/AuthScreen';
-import BottomRepairmen from './src/navigation/repairmen/BottomRepairmen';
 
 LogBox.ignoreAllLogs();
-const Stack = createNativeStackNavigator();
 
 export default function App() {
   const [loading, setLoading] = useState(true);
@@ -56,12 +31,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ffffff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
